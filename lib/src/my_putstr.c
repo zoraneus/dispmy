db87ae49c2f8e142017f9d4db37f67bc91ccb10c
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2019-02-09 02:08:29 +0100

    static strlen

--- lib/src/my_putstr.c
+++ lib/src/my_putstr.c
@@ -7,6 +7,15 @@
 
 #include "my_display.h"
 
+static int s_strlen(char const *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
 void my_putstr(char const *str)
 {
     write(1, str, s_strlen(str));
